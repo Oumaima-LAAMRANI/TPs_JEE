203a1ab13dc2759cb990648298d40514b079fa60
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-04-16 19:51:28 +0200

    Password Encoder

--- patients-mvc/src/main/java/me/laamrani/oumaima/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/me/laamrani/oumaima/PatientsMvcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.xml.crypto.Data;
 import java.util.Date;
@@ -31,5 +33,9 @@ public class PatientsMvcApplication {
 			);
 
 		};
+	}
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 }
