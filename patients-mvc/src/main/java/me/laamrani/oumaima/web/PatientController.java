acdb2f3d16ad992666492effe56fe63f68e4fa87
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-04-16 18:04:46 +0200

    Edit patient

--- patients-mvc/src/main/java/me/laamrani/oumaima/web/PatientController.java
+++ patients-mvc/src/main/java/me/laamrani/oumaima/web/PatientController.java
@@ -53,10 +53,19 @@ public class PatientController {
         return "formPatients";
     }
     @PostMapping(path="/save")
-    public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
         if(bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
-        return "redirect:/formPatients";
+        return "redirect:/index?page="+page+"&keyword="+keyword;
     }
-
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,Long id, String keyword, int page){
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("Patient intouvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
 }
