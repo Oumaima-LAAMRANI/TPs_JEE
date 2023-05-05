055adcc33edc21dc1ffaf8420bb305c9d814d35d
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-05-05 08:12:24 +0200

    BankAccountRepository interface based on Spring Data

--- bank-account-service/src/main/java/org/sid/bankaccountservice/repositories/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sid.bankaccountservice.repositories;
-
-import org.sid.bankaccountservice.entities.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
-}
