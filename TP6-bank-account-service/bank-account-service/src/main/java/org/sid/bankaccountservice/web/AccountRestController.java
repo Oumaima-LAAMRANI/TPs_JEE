c5aba0498c9ee79927f75476b9acbd1a3607401b
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-05-05 19:39:19 +0200

    Rest controller

--- TP6-bank-account-service/bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
+++ TP6-bank-account-service/bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
@@ -2,13 +2,14 @@ package org.sid.bankaccountservice.web;
 
 import org.sid.bankaccountservice.entities.BankAccount;
 import org.sid.bankaccountservice.repositories.BankAccountRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class AccountRestController {
     private BankAccountRepository bankAccountRepository;
 
@@ -24,5 +25,22 @@ public class AccountRestController {
         return bankAccountRepository.findById(id)
                 .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
     }
-
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount){
+       if (bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
+        return  bankAccountRepository.save(bankAccount);
+    }
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount){
+       BankAccount account=bankAccountRepository.findById(id).orElseThrow();
+       if (bankAccount.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+       if (bankAccount.getCreatAt()!=null) account.setCreatAt(new Date());
+       if (bankAccount.getType()!=null) account.setType(bankAccount.getType());
+       if (bankAccount.getCurrency()!=null) account.setCurrency(bankAccount.getCurrency());
+       return  bankAccountRepository.save(account);
+    }
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount(@PathVariable String id){
+        bankAccountRepository.deleteById(id);
+    }
 }
